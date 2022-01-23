a067b701f4969b6a8d06a6e5e015a0f3b2874d48
Guru <guru.h121@gmail.com>
2022-01-23 14:39:55 +0530

    deploy error solved

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,25 +14,10 @@ const Index = (props) => {
   );
 };
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const result = await axios.get("http://localhost:3000/api/searchPhoto");
 
-  const arrayOfPromise = [];
-  result.data.mappedResponse.forEach((a) => {
-    const promise = new Promise((resolve) => {
-      Jimp.read(a.urls.regular).then((data) => {
-        a.imageProps = {
-          width: data.bitmap.width,
-          height: data.bitmap.height,
-        };
-        resolve();
-      });
-    });
-    arrayOfPromise.push(promise);
-  });
-  await Promise.all(arrayOfPromise);
-
-  return { props: { result: result.data } };
+  return { props: { result: result.data }, revalidate: 60 * 60 * 24 };
 };
 
 export default Index;
